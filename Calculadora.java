71df278b0829db41e6d88ee1d8013765ddc40873
irma660 <1325455@senati.pe>
2023-08-17 15:15:39 -0500

    lo modiqué

---
+++ Calculadora.java
@@ -0,0 +1,11 @@
+
+package Calculadora;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        System.out.println("todo empiesa aqui");
+        System.out.println("Otra vez");
+    }
+    
+}
